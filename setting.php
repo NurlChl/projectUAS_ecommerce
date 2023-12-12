5389a21ea2d768f7a6339dc31cb837c134543b37
NurlChl <nurul.cholil2373@gmail.com>
2023-12-12 19:15:52 +0700

    perubahan produk terbaru

--- setting.php
+++ setting.php
@@ -36,7 +36,7 @@ if (isset($_POST["cari"])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <title>Setting</title>
     <link rel="stylesheet" href="setting.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
